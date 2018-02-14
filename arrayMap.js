9b4bc1692f9c16345599e4b7d46b2b7eda36de45
Harry <harrylll93@gmail.com>
2018-02-14 23:14:39 +0000

    Added arrayMap exercise

---
+++ arrayMap.js
@@ -0,0 +1,15 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function(obj) {
+  let sqrZ = obj.x * obj.x + obj.y * obj.y;
+  let answer = Math.sqrt(sqrZ);
+
+  return answer;
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
